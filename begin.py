f558fb78d08d401ae586a5a379b0271e26ead559
Vidhur <vidhurvashisht@gmail.com>
2018-10-20 11:23:27 -0400

    Add files via upload

---
+++ begin.py
@@ -0,0 +1,26 @@
+#This is an inputted string
+print("Hello, World!")
+
+#This is an inequality
+if 5 > 2:
+    print("Five is greater than two!")
+
+#This is how you create and print variables
+x = 6
+y = "Sherlock Holmes"
+print(x, "The dopest detective is " + y)
+
+#overriding
+x = 3
+x = "Crimson Flames of Passion"
+print(x)
+
+#math things and what not
+x = 5
+y = 17.890
+
+print(x - y)
+print(x + y)
+
+
+exit() 
