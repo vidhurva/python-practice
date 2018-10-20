ba357e2c2869c67fb1d88040c2aba55a2028d1d1
Vidhur <vidhurvashisht@gmail.com>
2018-10-20 11:58:29 -0400

    Add files via upload

---
+++ begin2.py
@@ -0,0 +1,24 @@
+# How to print what type of function is conducted
+x = 1
+y = 1.85
+z = "Chandler Bing"
+zz = 5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(zz))
+
+#How to change a variable type
+y = int(y)
+
+print(y)
+
+a = " When's Lunch? What's for lunch? "
+
+print(a)
+print(a[2], a[3], a[4], a[6])
+print(a[2:5], a[6])
+print(a.strip())
+
+exit()
