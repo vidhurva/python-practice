ef33a8bd18e1af6dfaf0092cfe3315b9ca969b21
Vidhur <vidhurvashisht@gmail.com>
2018-10-27 10:03:40 -0400

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+a = "New York is my Campus *sigh* ...   "
+print(a[17])
+
+#removes any whitespace from beginning to End
+print(a.strip())
+
+b = "New York is my Campus *sigh* ...   "
+print(b[0:2])
+
+#prints the length of the string
+print(len(b))
+
+print(a.replace("g", "n") + a.replace("h", "g"))
+
+print(a.split("*"))
